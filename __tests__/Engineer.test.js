9333649a09091d44b18116412464144ad3a78247
omcewan <orlandomcewan231@gmail.com>
2022-02-12 14:51:47 -0500

    created the Engineer class and wrote the test to test its functionality

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,69 @@
 const Engineer = require("../lib/Engineer");
+
+describe("test if inherited properties and methods still function as they should", () => {
+  test("check if class is inheriting from parent class Employee", () => {
+    const engineer = new Engineer("Orlando");
+    engineer.email = "gmail.com";
+
+    expect(engineer.name).toBe("Orlando");
+    expect(engineer.id).toEqual(expect.any(Number));
+    expect(engineer.email).toEqual(expect.any(String));
+  });
+
+  test("get the name of the engineer", () => {
+    const engineer = new Engineer("Orlando");
+
+    expect(engineer.getName()).toEqual(
+      expect.stringContaining(engineer.name.toString())
+    );
+  });
+
+  test("create a generator object", () => {
+    const engineer = new Engineer("Orlando");
+    expect(engineer.idGenerator()).toEqual(expect.any(Object));
+  });
+
+  test("check if generator object contains a property called 'value'", () => {
+    const engineer = new Engineer("Orlando");
+    expect(engineer.idGenerator().next()).toHaveProperty("value");
+  });
+
+  test("get the id of the engineer", () => {
+    const engineer = new Engineer("Orlando");
+    engineer.id = 0;
+
+    expect(engineer.getId()).toEqual(
+      expect.stringContaining(engineer.id.toString())
+    );
+  });
+
+  test("get the email of the engineer", () => {
+    const engineer = new Engineer("Orlando");
+    engineer.email = "gmail.com";
+
+    expect(engineer.getEmail()).toEqual(
+      expect.stringContaining(engineer.email.toString())
+    );
+  });
+});
+
+describe("check fucntionalitiy of Engineer class specific properties and funcitons", () => {
+  test("return the Engineer's github username", () => {
+    const engineer = new Engineer("Orlando");
+    engineer.github = "omcewan";
+    expect(engineer.github).toEqual(expect.any(String));
+  });
+
+  test("returns 'Engineer' ", () => {
+    const engineer = new Engineer("Orlando");
+
+    expect(engineer.getRole()).toBe("Engineer");
+  });
+
+  test("get the engineer's github link", () => {
+    const engineer = new Engineer("Orlando");
+    engineer.github = "omcewan";
+
+    expect(engineer.gitHub()).toEqual(expect.stringContaining(engineer.github.toString()))
+  })
+});
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,58 +1,62 @@
 const Manager = require("../lib/Manager");
 
-test("check if class is inheriting from parent class Employee", () => {
-  const manager = new Manager("orlando");
-  manager.email = "gmail.com";
+describe("test if inherited properties and methods still function as they should", () => {
+  test("check if class is inheriting from parent class Employee", () => {
+    const manager = new Manager("Orlando");
+    manager.email = "gmail.com";
 
-  expect(manager.name).toBe("orlando");
-  expect(manager.id).toEqual(expect.any(Number));
-  expect(manager.email).toEqual(expect.any(String));
+    expect(manager.name).toBe("Orlando");
+    expect(manager.id).toEqual(expect.any(Number));
+    expect(manager.email).toEqual(expect.any(String));
+  });
+
+  test("get the name of the manager", () => {
+    const manager = new Manager("Orlando");
+
+    expect(manager.getName()).toEqual(
+      expect.stringContaining(manager.name.toString())
+    );
+  });
+
+  test("create a generator object", () => {
+    const manager = new Manager("Orlando");
+    expect(manager.idGenerator()).toEqual(expect.any(Object));
+  });
+
+  test("check if generator object contains a property called 'value'", () => {
+    const manager = new Manager("Orlando");
+    expect(manager.idGenerator().next()).toHaveProperty("value");
+  });
+
+  test("get the id of the manager", () => {
+    const manager = new Manager("Orlando");
+    manager.id = 0;
+
+    expect(manager.getId()).toEqual(
+      expect.stringContaining(manager.id.toString())
+    );
+  });
+
+  test("get the email of the manager", () => {
+    const manager = new Manager("Orlando");
+    manager.email = "gmail.com";
+
+    expect(manager.getEmail()).toEqual(
+      expect.stringContaining(manager.email.toString())
+    );
+  });
 });
 
-test("get the name of the manager", () => {
-  const manager = new Manager("orlando");
+describe("check fucntionalitiy of Manager class specific properties and funcitons", () => {
+  test("returns 'Manager' ", () => {
+    const manager = new Manager("Orlando");
 
-  expect(manager.getName()).toEqual(
-    expect.stringContaining(manager.name.toString())
-  );
-});
+    expect(manager.getRole()).toBe("Manager");
+  });
 
-test("create a generator object", () => {
-  const manager = new Manager("Orlando");
-  expect(manager.idGenerator()).toEqual(expect.any(Object));
-});
+  test("returns managers's office number", () => {
+    const manager = new Manager("Orlando");
 
-test("check if generator object contains a property called 'value'", () => {
-  const manager = new Manager("Orlando");
-  expect(manager.idGenerator().next()).toHaveProperty("value");
-});
-
-test("get the id of the manager", () => {
-  const manager = new Manager("orlando");
-  manager.id = 0;
-
-  expect(manager.getId()).toEqual(
-    expect.stringContaining(manager.id.toString())
-  );
-});
-
-test("get the email of the manager", () => {
-  const manager = new Manager("orlando");
-  manager.email = "gmail.com";
-
-  expect(manager.getEmail()).toEqual(
-    expect.stringContaining(manager.email.toString())
-  );
-});
-
-test("returns 'Manager' ", () => {
-  const manager = new Manager("orlando");
-
-  expect(manager.getRole()).toBe("Manager");
-});
-
-test("returns managers's office number", () => {
-  const manager = new Manager("orlando");
-
-  expect(manager.officeNumber).toEqual(expect.any(Number));
+    expect(manager.officeNumber).toEqual(expect.any(Number));
+  });
 });
